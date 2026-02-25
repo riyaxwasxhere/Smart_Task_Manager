344b833d9d321533023d192d59cd9a4d9db45a2b
Riya Dutta <riyapc1406@gmail.com>
2026-02-25 23:50:18 +0530

    feat: added expo folder structure

---
+++ frontend/app/index.tsx
@@ -0,0 +1,15 @@
+import { Text } from "@react-navigation/elements"
+import { View } from "react-native-reanimated/lib/typescript/Animated"
+import "../global.css"
+
+
+const Index = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'red' }}>
+      <Text>Welcomeeeeeeeeeeeeeeee</Text>
+      
+    </View>
+  )
+}
+
+export default Index
