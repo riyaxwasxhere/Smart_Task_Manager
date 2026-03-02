de9dd0c889fd51835455f7a5929b54feb727d615
Riya Dutta <riyapc1406@gmail.com>
2026-03-03 00:23:21 +0530

    ..

--- frontend/api/axios.js
+++ frontend/api/axios.js
@@ -2,7 +2,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage'
 import axios from 'axios'
 
 const API = axios.create({
-    baseURL: "http://192.168.1.3:8000/api"
+    baseURL: "https://smart-task-manager-1-jj8r.onrender.com/api"
 })
 
 API.interceptors.request.use(async(config) =>{
